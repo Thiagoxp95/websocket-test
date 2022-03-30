15d31ee3a4e10a0a073f9ffd80ed652473447daa
Thiago Pereira <thiago.pereira@ontariotechu.net>
2022-03-30 17:22:34 -0400

    fix

--- index.js
+++ index.js
@@ -3,23 +3,33 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  cors: {
+    origin:"http://localhost:3001",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log('Server listening at port %d', port);
+  console.log('Server listening at port %d', port );
 });
 
-// Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Chatroom
 
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
 
-  // get query params
-  const query = socket.handshake.query;
+  console.log("connectado");  
 
-  //send back the query params
-  socket.emit('hello', "Hello from server");
+  setTimeout(() => {
+    socket.emit('message', {
+      message: 'Hello World'
+    });
+  });
+
 });
+
+
